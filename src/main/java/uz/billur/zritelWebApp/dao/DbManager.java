02978f130de83fd05592efd2ea1e33c9844b0f11
Ahmadjon <83345725+tuhtaboyev@users.noreply.github.com>
2025-03-16 13:05:40 +0500

    Delete src/main/java/uz/billur/zritelWebApp directory

--- src/main/java/uz/billur/zritelWebApp/dao/DbManager.java
+++
@@ -1,8 +0,0 @@
-package uz.billur.zritelWebApp.dao;
-
-import java.io.IOException;
-import java.sql.Connection;
-
-public interface DbManager {
-    public Connection MySqlAloqa() throws IOException;
-}
